95cc922189209bd26bff062d400483b1343b3f01
ericflatt <eflatt18@gmail.com>
2021-05-15 13:43:03 -0400

    commit

---
+++ src/components/hero.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class Hero extends React.Component {
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div className="heroBG">
+                <h1>hello</h1>
+            </div>
+        );
+    }
+}
+
+export default Hero;
